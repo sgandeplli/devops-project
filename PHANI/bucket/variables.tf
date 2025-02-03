f8c28b9f98d26fbdb10efbd34dee6abd6fdeee89
sgandeplli <sgandepalli@softility.com>
2025-02-03 12:42:55 +0530

    Update variables.tf

--- PHANI/bucket/variables.tf
+++ PHANI/bucket/variables.tf
@@ -1,48 +1,19 @@
-variable "project_id" {
+variable "project" {
   description = "The GCP project ID"
   type        = string
-  default     = "saitejaameda"
 }
 
 variable "region" {
-  description = "The GCP region"
+  description = "The region in which to create resources"
   type        = string
-  default     = "us-central1"
-}
-
-variable "credentials_file" {
-  description = "Path to the GCP credentials JSON file"
-  type        = string
-  default     = "gcp.json"
 }
 
 variable "bucket_name" {
-  description = "The name of the GCS bucket"
+  description = "The name of the storage bucket"
   type        = string
-  default     = "my-terraform-gcs-bucket"
 }
 
 variable "bucket_location" {
-  description = "The location of the GCS bucket"
+  description = "The location of the storage bucket"
   type        = string
-  default     = "US"
 }
-
-variable "storage_class" {
-  description = "The storage class of the GCS bucket"
-  type        = string
-  default     = "STANDARD"
-}
-
-variable "versioning_enabled" {
-  description = "Enable versioning on the bucket"
-  type        = bool
-  default     = true
-}
-
-variable "object_lifecycle_days" {
-  description = "Number of days before objects are deleted"
-  type        = number
-  default     = 30
-}
-
