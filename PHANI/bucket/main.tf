e92aa3bf506228fc4800938f4b4c30cd946f1137
sgandeplli <sgandepalli@softility.com>
2025-02-03 13:30:57 +0530

    Update main.tf

--- PHANI/bucket/main.tf
+++ PHANI/bucket/main.tf
@@ -8,11 +8,3 @@ resource "google_storage_bucket" "bucket" {
   location = var.bucket_location
   storage_class = var.storage_class
 }
-
-output "bucket_name" {
-  value = google_storage_bucket.bucket.name
-}
-
-output "bucket_url" {
-  value = "gs://${google_storage_bucket.bucket.name}"
-}
