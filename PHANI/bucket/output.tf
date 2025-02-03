e66316d99b8d0df7c5b136bb69cbae3fa676e39c
sgandeplli <sgandepalli@softility.com>
2025-02-03 12:43:15 +0530

    Update output.tf

--- PHANI/bucket/output.tf
+++ PHANI/bucket/output.tf
@@ -1,19 +1,4 @@
-variable "project" {
-  description = "The GCP project ID"
-  type        = string
-}
-
-variable "region" {
-  description = "The region in which to create resources"
-  type        = string
-}
-
-variable "bucket_name" {
+output "bucket_name" {
   description = "The name of the storage bucket"
-  type        = string
-}
-
-variable "bucket_location" {
-  description = "The location of the storage bucket"
-  type        = string
+  value       = google_storage_bucket.my_bucket.name
 }
