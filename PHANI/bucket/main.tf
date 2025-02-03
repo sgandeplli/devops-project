402e8dc3076247e7e84b1c1b7d8bc15178ce8eb4
sgandeplli <sgandepalli@softility.com>
2025-02-03 13:23:54 +0530

    Update main.tf

--- PHANI/bucket/main.tf
+++ PHANI/bucket/main.tf
@@ -1,12 +1,18 @@
-resource "google_storage_bucket" "my_bucket" {
+provider "google" {
+  project = var.project_id
+  region  = var.region
+}
+
+resource "google_storage_bucket" "bucket" {
   name     = var.bucket_name
   location = var.bucket_location
+  storage_class = var.storage_class
+}
 
-  versioning {
-    enabled = true
-  }
+output "bucket_name" {
+  value = google_storage_bucket.bucket.name
+}
 
-  lifecycle {
-    prevent_destroy = true
-  }
+output "bucket_url" {
+  value = "gs://${google_storage_bucket.bucket.name}"
 }
